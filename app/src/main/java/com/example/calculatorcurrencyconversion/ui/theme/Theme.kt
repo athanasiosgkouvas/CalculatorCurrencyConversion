06914e57b33ac9c8c5fc729737d7723d6ad689e9
Athanasios Gkouvas <gkouvas@outlook.com>
2023-07-01 00:36:00 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/calculatorcurrencyconversion/ui/theme/Theme.kt
@@ -0,0 +1,107 @@
+package com.example.calculatorcurrencyconversion.ui.theme
+
+import android.app.Activity
+import android.os.Build
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
+
+private val darkColorScheme = darkColorScheme(
+    primary = darkPrimary,
+    onPrimary = darkOnPrimary,
+    primaryContainer = darkPrimaryContainer,
+    onPrimaryContainer = darkOnPrimaryContainer,
+    inversePrimary = darkPrimaryInverse,
+    secondary = darkSecondary,
+    onSecondary = darkOnSecondary,
+    secondaryContainer = darkSecondaryContainer,
+    onSecondaryContainer = darkOnSecondaryContainer,
+    tertiary = darkTertiary,
+    onTertiary = darkOnTertiary,
+    tertiaryContainer = darkTertiaryContainer,
+    onTertiaryContainer = darkOnTertiaryContainer,
+    error = darkError,
+    onError = darkOnError,
+    errorContainer = darkErrorContainer,
+    onErrorContainer = darkOnErrorContainer,
+    background = darkBackground,
+    onBackground = darkOnBackground,
+    surface = darkSurface,
+    onSurface = darkOnSurface,
+    inverseSurface = darkInverseSurface,
+    inverseOnSurface = darkInverseOnSurface,
+    surfaceVariant = darkSurfaceVariant,
+    onSurfaceVariant = darkOnSurfaceVariant,
+    outline = darkOutline
+)
+
+private val lightColorScheme = lightColorScheme(
+    primary = lightPrimary,
+    onPrimary = lightOnPrimary,
+    primaryContainer = lightPrimaryContainer,
+    onPrimaryContainer = lightOnPrimaryContainer,
+    inversePrimary = lightPrimaryInverse,
+    secondary = lightSecondary,
+    onSecondary = lightOnSecondary,
+    secondaryContainer = lightSecondaryContainer,
+    onSecondaryContainer = lightOnSecondaryContainer,
+    tertiary = lightTertiary,
+    onTertiary = lightOnTertiary,
+    tertiaryContainer = lightTertiaryContainer,
+    onTertiaryContainer = lightOnTertiaryContainer,
+    error = lightError,
+    onError = lightOnError,
+    errorContainer = lightErrorContainer,
+    onErrorContainer = lightOnErrorContainer,
+    background = lightBackground,
+    onBackground = lightOnBackground,
+    surface = lightSurface,
+    onSurface = lightOnSurface,
+    inverseSurface = lightInverseSurface,
+    inverseOnSurface = lightInverseOnSurface,
+    surfaceVariant = lightSurfaceVariant,
+    onSurfaceVariant = lightOnSurfaceVariant,
+    outline = lightOutline
+)
+
+@Composable
+fun CalculatorCurrencyConversionTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
+    dynamicColor: Boolean = true,
+    content: @Composable () -> Unit
+) {
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+        }
+
+        darkTheme -> darkColorScheme
+        else -> lightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+        }
+    }
+
+    MaterialTheme(
+        colorScheme = colorScheme,
+        typography = Typography,
+        shapes = shapes,
+        content = content
+    )
+}
